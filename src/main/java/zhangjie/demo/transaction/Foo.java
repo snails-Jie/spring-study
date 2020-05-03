f11977d517952067bcc7021c55d0470354e8d07c
zhangjie <745012773@qq.com>
2020-05-03 18:09:50 +0800

    spring事务

---
+++ src/main/java/zhangjie/demo/transaction/Foo.java
@@ -0,0 +1,13 @@
+package zhangjie.demo.transaction;
+
+public class Foo {
+    private String fooName;
+
+    public String getFooName() {
+        return fooName;
+    }
+
+    public void setFooName(String fooName) {
+        this.fooName = fooName;
+    }
+}
---
+++ src/main/java/zhangjie/demo/transaction/FooService.java
@@ -0,0 +1,11 @@
+package zhangjie.demo.transaction;
+
+public interface FooService {
+    Foo getFoo(String fooName);
+
+    Foo getFoo(String fooName, String barName);
+
+    void insertFoo(Foo foo);
+
+    void updateFoo(Foo foo);
+}
